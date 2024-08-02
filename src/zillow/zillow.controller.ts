c2778131c4856c5180777d717b3b25461b6183f4
Yiting Xiao <yitingxiao0531@gmail.com>
2024-08-02 11:41:18 -0400

    Feat: add get route to get property search results from Zillow API

--- src/zillow/zillow.controller.ts
+++ src/zillow/zillow.controller.ts
@@ -3,4 +3,21 @@ import { ZillowService } from './zillow.service';
 
 @Controller('zillow')
 export class ZillowController {
+    constructor(private zillowService: ZillowService) {}
+    /**
+     * Search for filtered properties by neighborhood, city, or ZIP code. PS : To search for an address of a property, use the "/search_address" endpoint. 
+     * For a list of properties, you can select the output format (JSON , CSV , XLSX) using the optional "output" parameter.
+     * @param location 
+     * @param status 
+     * @param sortSelection 
+     * @returns 
+     */
+    @Get('search')
+    async searchProperties(
+        @Query('location') location: string,
+        @Query('status') status: string = 'forSale',
+        @Query('sortSelection') sortSelection: string = 'priorityscore'
+    ) {
+        return this.zillowService.searchProperties(location, status, sortSelection);
+    }
 }
--- src/zillow/zillow.service.ts
+++ src/zillow/zillow.service.ts
@@ -1,6 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class ZillowService {
-    
+    private readonly RAPIDAPI_KEY = process.env.ZILLOW_API_KEY;
+    private readonly ZILLOW_API_BASE_URL = process.env.ZILLOW_API_BASE_URL;
+    private readonly ZILLOW_API_HOST = process.env.ZILLOW_API_HOST;
+
+    async searchProperties(location: string, status: string = 'forSale', sortSelection: string = 'priorityscore') {
+        try {
+            const response = await axios.get(`${this.ZILLOW_API_BASE_URL}/search`, {
+                params: {
+                    location,
+                    output: 'json',
+                    status,
+                    sortSelection
+                },
+                headers: {
+                    'X-RapidAPI-Host': this.ZILLOW_API_HOST,
+                    'X-RapidAPI-Key': this.RAPIDAPI_KEY,
+                },
+            });
+            console.log('API Response:', response.data);
+            return response.data;
+        } catch (error) {
+            console.error('Error in ZillowService:', error.response ? error.response.data : error.message);
+            throw error;
+        }
+    }
 }
