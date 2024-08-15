25d02293a74148d57f922634c85c7965d2c97eb3
Yiting Xiao <yitingxiao0531@gmail.com>
2024-08-15 14:42:26 -0400

    Feat: add container

--- src/zillow/DBservices/property.service.ts
+++ src/zillow/DBservices/property.service.ts
@@ -34,6 +34,7 @@ export class PropertyService {
                   taxAssessedValue: property.taxAssessedValue ?? 0,
                   zestimate: property.zestimate ?? 0,
                   zipcode: property.zipcode ?? 'Unknown',
+                  createdAt: new Date()
               },
               create: {
                   bathrooms: property.bathrooms ?? 0,
@@ -58,6 +59,7 @@ export class PropertyService {
                   zestimate: property.zestimate ?? 0,
                   zipcode: property.zipcode ?? 'Unknown',
                   zpid: property.zpid,
+                  createdAt: new Date()
               },
     
           });
